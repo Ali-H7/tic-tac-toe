f93c0cc90dfa4084fb108943bedf1a30ae7d8c46
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-07-16 07:06:29 +0300

    Get user input
    The interface dispay input field for the user to take his name

--- script.js
+++ script.js
@@ -13,24 +13,24 @@
 //     return {board, display, player1, player2};
 // })();
  
-// const players = (function playerInfo() {
-//     const playerOne = prompt("Player 1, What is your name?");
-//     const playerTwo = prompt("Player 2, What is your name?");
-//     const oneOrTwo = Math.floor(Math.random() * 2) + 1;
-//     const playerOneMark = oneOrTwo === 1 ? "O" : "X";
-//     const playerTwoMark = playerOneMark === "X" ? "O" : "X";
+function playerInfo() {
+    const {playerOneName, playerTwoName} = getPlayerNames()
+    // const playerOne = playerOneName
+    // const playerTwo = playerTwoName
+    const oneOrTwo = Math.floor(Math.random() * 2) + 1;
+    const playerOneMark = oneOrTwo === 1 ? "O" : "X";
+    const playerTwoMark = playerOneMark === "X" ? "O" : "X";
 
-//     function createPlayer (playerName, playerMark) {
-//         const player = playerName;
-//         const mark = playerMark;
-//         return {player , mark};
-//     };
+    function createPlayer (playerName, playerMark) {
+        const player = playerName;
+        const mark = playerMark;
+        return {player , mark};
+    };
 
-//     const player1 = createPlayer(playerOne, playerOneMark);
-//     const player2 = createPlayer(playerTwo, playerTwoMark);  
-
-//     return {player1, player2};
-// })();
+    const player1 = createPlayer(playerOneName, playerOneMark);
+    const player2 = createPlayer(playerTwoName, playerTwoMark);  
+    return {player1, player2};
+};
 
 // const turn = (function() {
 //     let playerChoice;
@@ -106,3 +106,51 @@
 // }
 
 // gameController();
+
+
+  function getPlayerNames() {
+    const playerOneName = document.querySelector("#player1-name").value;
+    const playerTwoName = document.querySelector("#player2-name").value;
+    return {playerOneName, playerTwoName}; 
+    };
+
+function displayNameMark() {
+    const {player1} = playerInfo();
+    const {player2} = playerInfo(); 
+    const player1Name = player1.player
+    const player2Name = player2.player
+    const player1Mark = player1.mark
+    const player2Mark = player2.mark
+
+    function createPlayersElement(playerName, mark, card) {
+            const cardElement = document.querySelector(card);
+            const divContainer = document.createElement("div");
+            const playerNameElement = document.createElement("p");
+            const playerMarkElement = document.createElement("p");
+
+            playerNameElement.textContent = playerName;
+            playerMarkElement.textContent = mark;
+            divContainer.appendChild(playerNameElement);
+            divContainer.appendChild(playerMarkElement);
+            cardElement.appendChild(divContainer);
+    }
+
+    const player1Element = createPlayersElement(player1Name, player1Mark, "#player1")
+    const player2Element = createPlayersElement(player2Name, player2Mark, "#player2")
+};
+
+
+ (function events () {
+    const dialog = document.querySelector("dialog");
+    window.addEventListener("load", () => {
+    dialog.showModal();
+  });
+
+    const startBtn = document.querySelector("#start-button");
+    startBtn.addEventListener ("click", () => {
+    dialog.close();
+    playerInfo()
+    displayNameMark()
+ });
+
+ })();
