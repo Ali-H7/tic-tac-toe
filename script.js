1c0ad17412cd456c7788787ea1cef3f503def317
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-07-17 19:33:54 +0300

    Refactor the code
    The code was getting messy and difficult to understand

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const gameBoard = (function(){
+const gameBoard = (function (){
     let board = [1,2,3,4,5,6,7,8,9];
-    let player1 = [];
-    let player2 = [];
+    let player1Board = [];
+    let player2Board = [];
 
     function display() {
         console.log("------");
@@ -11,9 +11,10 @@ const gameBoard = (function(){
     }
 
     function resetBoard() {
-      gameBoard.board = [1,2,3,4,5,6,7,8,9];
-      gameBoard.player1 = [];
-      gameBoard.player2 = [];
+      board.length = 0;
+      board.push(1,2,3,4,5,6,7,8,9);
+      player1.length = 0;
+      player2.length = 0;
 
       const squares = document.querySelectorAll(".square");
       squares.forEach (square => {
@@ -21,61 +22,48 @@ const gameBoard = (function(){
       })
     }
 
-    return {board, display, player1, player2, resetBoard};
-})();
- 
-function playerInfo() {
-    // const {playerOneName, playerTwoName} = getPlayerNames()
-    // // const playerOne = playerOneName
-    // // const playerTwo = playerTwoName
-    const playerOneName = document.querySelector("#player1-name").value;
-    const playerTwoName = document.querySelector("#player2-name").value;
-    // const oneOrTwo = Math.floor(Math.random() * 2) + 1;
-    const playerOneMark = "X"
-    const playerTwoMark = "O"
-    // const playerOneMark = oneOrTwo === 1 ? "O" : "X";
-    // const playerTwoMark = playerOneMark === "X" ? "O" : "X";
-
-    function createPlayer (playerName, playerMark) {
-        const player = playerName;
-        const mark = playerMark;
-        return {player , mark};
-    };
-
-    const player1 = createPlayer(playerOneName, playerOneMark);
-    const player2 = createPlayer(playerTwoName, playerTwoMark);  
-    return {player1, player2};
-};
-
-const turn = (function() {
-    let playerChoice;
-    let currentTurn = 1;
-    const {checkWinner, continueGame} = gameCondition();
-    const {player1} = playerInfo();
-    const {player2} = playerInfo(); 
-    return function playersTurn(htmlSquare) {
-        if (currentTurn == 1) {
-            currentTurn -= 1;
-            playerChoice = +htmlSquare.id;
-            gameBoard.player1.push(playerChoice);
-            gameBoard.board[playerChoice - 1] = player1.mark;
-            htmlSquare.textContent = player1.mark;
-            console.log(gameBoard.display())
-            checkWinner();
-        } else if (currentTurn == 0) {
-            currentTurn += 1;
-            let playerChoice = +htmlSquare.id;
-            gameBoard.player2.push(playerChoice);
-            gameBoard.board[playerChoice - 1] = player2.mark;
-            htmlSquare.textContent = player2.mark;
-            console.log(gameBoard.display())
-            checkWinner();
-        }
-        // continueGame();
-    };
+    return {board, display, player1Board, player2Board, resetBoard};
 })();
 
-function gameCondition() {
+function player() {
+
+    function getPlayerInfo(){
+        let player1Name = document.querySelector("#player1-name").value;
+        let player2Name = document.querySelector("#player2-name").value;
+        randomizeChoice = Math.floor(Math.random() * 2) + 1; // 1 or 2
+        let player1Mark = "X";
+        let player2Mark = "O";
+
+        function createPlayer (playerName, playerMark) {
+            const player = playerName;
+            const mark = playerMark;
+            return {player, mark};
+        };
+
+        const player1 = createPlayer(player1Name, player1Mark);
+        const player2 = createPlayer(player2Name, player2Mark);  
+        return {player1, player2};
+    };
+    
+    // HTML elements to display player Information
+    function createElements(playerName, playerMark, elementID) {
+            const cardElement = document.querySelector(elementID);
+            const divContainer = document.createElement("div");
+            const playerNameElement = document.createElement("p");
+            const playerMarkElement = document.createElement("p");
+
+            playerNameElement.textContent = playerName;
+            playerMarkElement.textContent = playerMark;
+            cardElement.classList.add("player-card");
+            divContainer.appendChild(playerNameElement);
+            divContainer.appendChild(playerMarkElement);
+            cardElement.appendChild(divContainer);
+    }
+
+    return {getPlayerInfo, createElements}; 
+}
+
+function game() {
     const winningCondition = {
         1: [1,2,3],
         2: [4,5,6],
@@ -86,111 +74,87 @@ function gameCondition() {
         7: [1,5,9],
         8: [3,5,7],
     };
-    let gameOver = false; 
 
-    function checkWinner(){
+    let gameOver = false;
+    let currentTurn = 1;
+    let playerChoice;
+    const {board, display, player1Board, player2Board, resetBoard} = gameBoard;
+    const {getPlayerInfo} = player();
+    playersInfo = getPlayerInfo();
+
+    function playTurn(playedLocation) {
+        if (currentTurn == 1) {
+            currentTurn -= 1;
+            playerChoice = +playedLocation.id;
+            player1Board.push(playerChoice);
+            board[playerChoice - 1] = playersInfo.player1.mark;
+            playedLocation.textContent = playersInfo.player1.mark;
+            checkGameState();
+            display();
+        } else if (currentTurn == 0) {
+            currentTurn += 1;
+            playerChoice = +playedLocation.id;
+            player2Board.push(playerChoice);
+            board[playerChoice - 1] = playersInfo.player2.mark;
+            playedLocation.textContent = playersInfo.player2.mark;
+            checkGameState();
+            display();
+        }
+    }
+
+    function checkGameState() {
         for (let i = 1; i < 9; i++) {
-            const checkPlayerOne = winningCondition[i].every((element) => gameBoard.player1.includes(element));
-            const checkPlayerTwo = winningCondition[i].every((element) => gameBoard.player2.includes(element));
-            if (checkPlayerOne) {
+            if (winningCondition[i].every((element) => player1Board.includes(element))) {
                 console.log("Player one is the Winner!");
                 gameOver = true;
-                gameBoard.display();
-            } else if (checkPlayerTwo) {
+            } else if (winningCondition[i].every((element) => player2Board.includes(element))) {
                 console.log("Player Two is the Winner!");
                 gameOver = true;
-                gameBoard.display();
             } 
         }
 
-        if (gameBoard.player1.length + gameBoard.player2.length == 9 && gameOver == false) {
+        if (player1Board.length + player2Board.length === 9 && gameOver === false) {
             console.log("It's a tie no one won!");
                 gameOver = true;
-                gameBoard.display();
         }
-        if (gameOver) {
-          gameOver = false;
-          const dialog = document.querySelector(".continue");
-          dialog.showModal();
-          const continueBtn = document.querySelector("#continue");
-          continueBtn.addEventListener ("click",()=> {
-            dialog.close();
-            gameBoard.resetBoard();
-            events.addEventListenerForBoard();
-          })
-        } 
+
+        if (gameOver === true) {
+            // TODO: RESET WHEN THE GAME IS OVER!
+            // gameOver = false;
+            // currentTurn = 1;
+          } 
     }
+    return {playTurn};
+}
 
-    return {checkWinner}
-};
-
-// function gameController() {
-//     gameBoard.display();
-//     turn.playersTurn();
-// }
-
-// gameController();
-
-function displayNameMark() {
-    const {player1} = playerInfo();
-    const {player2} = playerInfo(); 
-    const player1Name = player1.player
-    const player2Name = player2.player
-    const player1Mark = player1.mark
-    const player2Mark = player2.mark
-
-    function createPlayersElement(playerName, mark, elementID) {
-            const cardElement = document.querySelector(elementID);
-            const divContainer = document.createElement("div");
-            const playerNameElement = document.createElement("p");
-            const playerMarkElement = document.createElement("p");
-
-            playerNameElement.textContent = playerName;
-            playerMarkElement.textContent = mark;
-            cardElement.classList.add("player-card");
-            divContainer.appendChild(playerNameElement);
-            divContainer.appendChild(playerMarkElement);
-            cardElement.appendChild(divContainer);
-    }
-
-    const player1Element = createPlayersElement(player1Name, player1Mark, "#player1")
-    const player2Element = createPlayersElement(player2Name, player2Mark, "#player2")
-};
-
-
- const events = (function events () {
+const events = (function events () {
     const dialog = document.querySelector("dialog");
+
     window.addEventListener("load", () => {
     dialog.showModal();
   });
 
     const startBtn = document.querySelector("#start-button");
     startBtn.addEventListener ("click", () => {
-    dialog.close();
-    playerInfo();
-    displayNameMark();
-    addEventListenerForBoard();
+        const {getPlayerInfo, createElements} = player();
+
+        playersInfo = getPlayerInfo();
+        createElements(playersInfo.player1.player, playersInfo.player1.mark, "#player1");
+        createElements(playersInfo.player2.player, playersInfo.player2.mark, "#player2");
+        addEventListenerForBoard();
+        game();
+        dialog.close();
  });
 
  function addEventListenerForBoard() {
   const squares = document.querySelectorAll(".square");
+  const {playTurn} = game();
+
     squares.forEach(square => {
       square.addEventListener ("click", function squareClick() {
-        turn(square);
+        playTurn(square);
         square.removeEventListener("click", squareClick);
       });
     });
  };
- return {addEventListenerForBoard};
  })();
- 
- 
-
-//  function placeMark(square) {
-//     const {player1} = playerInfo();
-//     const {player2} = playerInfo();
-//     const player1Mark = player1.mark
-//     const player2Mark = player2.mark 
-//  }
-
-// Check for winner next
